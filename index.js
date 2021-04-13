3d3adf93e9875de7b288e9174ab76a8e080a873d
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-04-13 15:39:04 +0200

    Added mongodb

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const passphrase = process.env.PHRASE || "Matcher!"
+const { MongoClient } = require('mongodb');
+const uri = process.env.DATABASE_URI;
+const client = new MongoClient(uri, { useUnifiedTopology: true})
 
 app.get("/", (req, res) => {
   res.send(passphrase);
@@ -12,3 +15,13 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on localhost:${port}`)
 })
+
+async function main () {
+try {
+  await client.connect();
+} catch (e) {
+  console.error(e);
+}
+}
+
+main().catch(console.error)
