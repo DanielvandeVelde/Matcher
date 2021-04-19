c6ba6b8ff7dfb43547d8b0056a537ee41d4e9f0b
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-04-19 18:23:39 +0200

    handlebars

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 require('dotenv').config();
 
 const express = require("express");
+const exphbs = require("express-handlebars");
+const { MongoClient } = require('mongodb');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const passphrase = process.env.PHRASE || "Matcher!"
-const { MongoClient } = require('mongodb');
 const uri = process.env.DATABASE_URI;
-const client = new MongoClient(uri, { useUnifiedTopology: true})
+
+app.engine('.hbs', exphbs({extname:'.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(express.static('public'))
 
 app.get("/", (req, res) => {
-  res.send(passphrase);
+  const users = [
+    {name: "Name 1"}, 
+    {name: "Name 2"},
+    {name: "Name 3"}, 
+    {name: "Name 4"},
+    {name: "Name 5"}, 
+    {name: "Name 6"},
+  ];
+
+  res.render('home', { users });
 });
 
-app.listen(port, () => {
-    console.log(`Listening on localhost:${port}`)
+MongoClient.connect(uri, { useUnifiedTopology: true}, (err, client) => {
+  if(err) { 
+    throw err;
+  }
+
+  const db = client.db('matcher');
+  const users = db.collection('users');
+  app.locals.users = users;
 })
 
-async function main () {
-try {
-  await client.connect();
-} catch (e) {
-  console.error(e);
-}
-}
-
-main().catch(console.error)
+app.listen(port, () => console.log(`Listening on localhost:${port}`))
