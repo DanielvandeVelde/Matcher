9e67c87c6d55212a78f2379e9e8ec63c53b11e12
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-04-20 13:32:32 +0200

    handlebars and some parts set-up

--- index.js
+++ index.js
@@ -3,15 +3,36 @@ require('dotenv').config();
 const express = require("express");
 const exphbs = require("express-handlebars");
 const { MongoClient } = require('mongodb');
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport")
+const bodyParser = require("body-parser");
+const Strategy = require('passport-local').Strategy;
 
 const app = express();
 const port = process.env.PORT || 3000;
 const uri = process.env.DATABASE_URI;
+const secret = process.env.SECRET;
 
 app.engine('.hbs', exphbs({extname:'.hbs'}));
 app.set('view engine', '.hbs');
 
-app.use(express.static('public'))
+app.use(session ({
+  secret: secret,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+MongoClient.connect(uri, { useUnifiedTopology: true}, (err, client) => {
+  if(err) { 
+    throw err;
+  }
+
+  const db = client.db('matcher');
+  const users = db.collection('users');
+  app.locals.users = users;
+})
 
 app.get("/", (req, res) => {
   const users = [
@@ -26,14 +47,16 @@ app.get("/", (req, res) => {
   res.render('home', { users });
 });
 
-MongoClient.connect(uri, { useUnifiedTopology: true}, (err, client) => {
-  if(err) { 
-    throw err;
-  }
+app.get("/profile", (req, res) => {
+  res.render("profile")
+})
 
-  const db = client.db('matcher');
-  const users = db.collection('users');
-  app.locals.users = users;
+app.get("/login", (req, res) => {
+  res.render("login")
+})
+
+app.get("/register", (req, res) => {
+  res.render("register")
 })
 
 app.listen(port, () => console.log(`Listening on localhost:${port}`))
