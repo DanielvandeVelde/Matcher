b34722324a0506b5d203586b64771a8687888090
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-07 16:48:20 +0200

    spelling

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require("../controllers/AuthController.js");
+const auth = require("../controllers/authController.js");
 
 router.get('/', auth.home);
 
