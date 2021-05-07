9f1210dfcf57362377975390e57325ef0d01523b
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-07 09:58:48 +0200

    added auth routing with help of controller

---
+++ router/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const auth = require("../controllers/AuthController.js");
+
+router.get('/', auth.home);
+
+router.get('/register', auth.register);
+router.post('/register', auth.doRegister);
+
+router.get('/login', auth.login);
+router.post('/login', auth.doLogin);
+
+router.get('/logout', auth.logout);
+
+module.exports = router;
