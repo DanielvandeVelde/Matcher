58731ec6fa711aa611e655a13fdc72b099e44eb1
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-07 09:55:45 +0200

    mongoose user data model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new mongoose.Schema(
+    {
+    name: {
+        type: String
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number
+    },
+    date: { 
+        type: Date,
+        default: Date.now
+    }
+},{strict: false});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
