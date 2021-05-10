6400d26df98ecb9cb4e9ad756204c0ba3fcfd33f
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-10 11:47:53 +0200

    fixed map boundaries

--- public/js/overview.js
+++ public/js/overview.js
@@ -3,10 +3,14 @@ let marker;
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-   maxZoom: 18,
    id: 'mapbox/streets-v11',
    tileSize: 512,
    zoomOffset: -1,
+   noWrap: true,
+   bounds: [
+       [-90, -180],
+       [90, 180]
+     ],
    accessToken: 'pk.eyJ1IjoiZGFuZmllbGRlciIsImEiOiJja29lNnVvcHAwMmdpMnVsenJtN3MyMXd1In0.gqLtr0Oao-J5aqvMKwrNxw'
 }).addTo(mymap);
 
