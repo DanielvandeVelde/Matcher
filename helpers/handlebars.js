09f147437825d53cbaa2f9fe4ef19fa47be6b17f
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-11 21:53:30 +0200

    refactored helpers

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -1,26 +1,23 @@
-const handlebarsHelpers = {}
+const handlebarsHelpers = {};
 
-handlebarsHelpers.overviewMapMaker = content => {
-      const markers = content.map(item => {
-         return (`L.marker(${item.loc.coordinates[0]},${item.loc.coordinates[1]}).addTo(mymap);`)
-      })
-
-      return (
-      `<script>
+handlebarsHelpers.overviewMapMaker = (content) => {
+	return `<script>
      const addMarkers = () => {
       ${handlebarsHelpers.mapMarkers(content)}
       }
      addMarkers();
-      </script>`)
-   }
+      </script>`;
+};
 
-handlebarsHelpers.mapMarkers = content => {
-      let markers;
-      content.map((item) => {
-         if(item.loc.coordinates[0]) {
-         markers += (`marker = L.marker([${parseFloat(item.loc.coordinates[1])},${parseFloat(item.loc.coordinates[0])}]).addTo(mymap);`)
-      }})
-      return markers 
-}
+handlebarsHelpers.mapMarkers = (content) => {
+	let markers = "";
+	content.map((item) => {
+		markers += `marker = L.marker([
+				${parseFloat(item.loc.coordinates[1])},
+				${parseFloat(item.loc.coordinates[0])}
+			]).addTo(mymap); `;
+	});
+	return markers;
+};
 
 module.exports = handlebarsHelpers;
