46a7a5d882191bdf58e106251f0d0444f56376b3
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-31 14:15:40 +0200

    concat & compress css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 const gulp = require("gulp")
 const sass = require("gulp-sass")
+const concat = require("gulp-concat")
 
-gulp.task("sass", () => {
-  return gulp.src("src/scss/*").pipe(sass()).pipe(gulp.dest("dist/css"))
+gulp.task("css", () => {
+  return gulp
+    .src("src/scss/*")
+    .pipe(concat("main.css"))
+    .pipe(sass({ outputStyle: "compressed" }))
+    .pipe(gulp.dest("dist/css"))
 })
 
 gulp.task("watch", () => {
-  gulp.watch("src/scss/*", ["sass"])
+  gulp.watch("src/scss/*", ["css"])
 })
