f2c0c085046e70afe2781a73aeaf8227dcb32e8c
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-05-31 13:13:13 +0200

    created watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,9 @@ const gulp = require("gulp")
 const sass = require("gulp-sass")
 
 gulp.task("sass", () => {
-  return gulp
-    .src("src/scss/style.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("dist/css"))
+  return gulp.src("src/scss/*").pipe(sass()).pipe(gulp.dest("dist/css"))
+})
+
+gulp.task("watch", () => {
+  gulp.watch("src/scss/*", ["sass"])
 })
