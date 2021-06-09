f00f29147a2b1d1520a5c69789998e1296b71cb8
DanielVanDeVelde <velde.van.de.daniel@gmail.com>
2021-06-09 09:50:58 +0200

    fixed: merge conflicts

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.engine(
 )
 app.set("view engine", ".hbs")
 app.use(compression())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "dist")))
 app.use(express.urlencoded({ extended: false }))
 app.use(
   session({
