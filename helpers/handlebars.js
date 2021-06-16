85d2b9ccd1e14cc0f9514c53b7bdcb983bdd66eb
unknown <reinier.van.limpt@gmail.com>
2021-06-16 17:20:09 +0200

    an attempt to fix indentation in helpers file

--- helpers/handlebars.js
+++ helpers/handlebars.js
@@ -3,19 +3,19 @@ const handlebarsHelpers = {};
 handlebarsHelpers.mapFunctionalities = (content, handleClick, handleForm) => {
   return `<script>
      (() => {
-		const redIcon = new L.Icon({
-			iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-			shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-			iconSize: [25, 41],
-			iconAnchor: [12, 41],
-			popupAnchor: [1, -34],
-			shadowSize: [41, 41]
-		  });
-      ${handlebarsHelpers.mapMarkers(content)}
-	  ${handlebarsHelpers.mapClick(handleClick)}
-	  ${handlebarsHelpers.formHandling(handleForm)}
+       const redIcon = new L.Icon({
+         iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+         shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+         iconSize: [25, 41],
+         iconAnchor: [12, 41],
+         popupAnchor: [1, -34],
+         shadowSize: [41, 41]
+        });
+        ${handlebarsHelpers.mapMarkers(content)}
+        ${handlebarsHelpers.mapClick(handleClick)}
+        ${handlebarsHelpers.formHandling(handleForm)}
       }
-     )(mymap);
+      )(mymap);
       </script>`;
 };
 
@@ -26,21 +26,21 @@ handlebarsHelpers.mapMarkers = (content) => {
       content.map((item, i) => {
         if (i === 0) {
           markers += `marker = L.marker([
-					${parseFloat(item.loc.coordinates[1])},
-					${parseFloat(item.loc.coordinates[0])}
-				], {icon: redIcon}).addTo(mymap).bindPopup('You 🤠'); `;
+            ${parseFloat(item.loc.coordinates[1])},
+            ${parseFloat(item.loc.coordinates[0])}
+          ], {icon: redIcon}).addTo(mymap).bindPopup('You 🤠'); `;
         } else {
           markers += `marker = L.marker([
-					${parseFloat(item.loc.coordinates[1])},
-					${parseFloat(item.loc.coordinates[0])}
-				]).addTo(mymap).bindPopup('<a href="#user${i}">${item.name}</a>'); `;
+            ${parseFloat(item.loc.coordinates[1])},
+            ${parseFloat(item.loc.coordinates[0])}
+          ]).addTo(mymap).bindPopup('<a href="#user${i}">${item.name}</a>'); `;
         }
       });
     } else {
       markers += `marker = L.marker([
-			${parseFloat(content.loc.coordinates[1])},
-			${parseFloat(content.loc.coordinates[0])}
-		], {icon: redIcon}).addTo(mymap); `;
+        ${parseFloat(content.loc.coordinates[1])},
+        ${parseFloat(content.loc.coordinates[0])}
+      ], {icon: redIcon}).addTo(mymap); `;
     }
   }
   return markers;
@@ -52,28 +52,28 @@ handlebarsHelpers.mapClick = (handleClick) => {
     clickEvent = ` 
     const updateValues = (marker) => {
       let lat = document.getElementById("lat");
-			let lng = document.getElementById("lng");
+      let lng = document.getElementById("lng");
       let form = document.getElementById("form");
       lat.setAttribute("value", marker._latlng.lat);
-		  lng.setAttribute("value", marker._latlng.lng);
+      lng.setAttribute("value", marker._latlng.lng);
       updateProfile(form);
     }
-		const onMapClick = (e) => {
-			if (marker) {
-				mymap.removeLayer(marker);
-				marker = L.marker(e.latlng, {icon: redIcon}).addTo(mymap).on("click", onMarkerClick);
+    const onMapClick = (e) => {
+      if (marker) {
+        mymap.removeLayer(marker);
+        marker = L.marker(e.latlng, {icon: redIcon}).addTo(mymap).on("click", onMarkerClick);
         updateValues(marker)
-			} else {
-				marker = L.marker(e.latlng, {icon: redIcon}).addTo(mymap).on("click", onMarkerClick);
+      } else {
+        marker = L.marker(e.latlng, {icon: redIcon}).addTo(mymap).on("click", onMarkerClick);
         updateValues(marker)
-			}
-		};
-		mymap.on("click", onMapClick);
+      }
+    };
+    mymap.on("click", onMapClick);
 	
-		function onMarkerClick() {
-			marker = null;
-			mymap.removeLayer(this);
-		}`;
+    function onMarkerClick() {
+      marker = null;
+      mymap.removeLayer(this);
+    }`;
   }
   return clickEvent;
 };
